fa84aeccf588c9d7653d9aeb0fc3a4b43dc1d376
TerryFung <terryfung@TerryFungs-MacBook-Pro.local>
2021-03-02 13:10:51 +0800

    update setting

--- config.el
+++ config.el
@@ -21,13 +21,13 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "Source Code Pro" :size 18 :weight 'normal))
+(setq doom-font (font-spec :family "Source Code Pro" :size 16 :weight 'normal))
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 ;; Dark theme :
-;;(setq doom-theme 'doom-horizon)
-(setq doom-theme 'doom-monokai-pro)
+(setq doom-theme 'doom-horizon)
+;;(setq doom-theme 'doom-monokai-pro)
 ;;(setq doom-theme 'doom-dracula)
 ;;(setq doom-theme 'doom-nord)
 ;;
